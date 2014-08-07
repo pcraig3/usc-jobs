1f9349be81c23c1758d5685283bd29c4306b0daf
Paul Craig <pcraig3@uwo.ca>
2014-08-06 20:22:45 -0400

    getting the departments wasn't super-hard.

--- templates/usc_jobs-meta-usc_jobs-single.php
+++ templates/usc_jobs-meta-usc_jobs-single.php
@@ -1,32 +1,63 @@
 <?php
 
+$html_string    = "";
+$subhead        = "";
+
 /**
- 1. To retrieve the meta box data	- get_post_meta( $post->ID ) will return an array of all the meta field values.
- or if you know the field id of the value you want, you can do $value = get_post_meta( $post->ID, $field_id, true );
+echo '<pre>';
+echo var_dump( get_the_terms( get_the_ID(), 'departments' ) );
+echo '</pre>';
+**/
+
+/**
+echo '<pre>';
+echo var_dump( get_post_meta( get_the_ID() ) );
+echo '</pre>';
+**/
+
+$array_of_departments = get_the_terms( get_the_ID(), 'departments' );
+
+$html_string .= '<h3>' . __( 'Departments' , 'usc-jobs') . '</h3>';
+
+//if no departments are assigned, get_the_terms() returns 'false'
+if( false === $array_of_departments ) {
+
+    $html_string .= '<p>This Job does not fall under a Department.</p>';
+}
+else {
+
+    $html_string .= '<p>';
+
+    foreach( $array_of_departments as &$department) {
+
+        $html_string .= $department->name . ", ";
+
+    }
+    unset($department);
+
+    $html_string = trim($html_string, ", ");
+    $html_string .= '</p>';
+
+}
+
+/**
+1. To retrieve the meta box data	- get_post_meta( $post->ID ) will return an array of all the meta field values.
+or if you know the field id of the value you want, you can do $value = get_post_meta( $post->ID, $field_id, true );
 
  * Possible Meta Values (useless ones are indented)
-        [_edit_last] => 1
-    [job_description] => Make bread cats can't remove.
-    [apply_by_date] => 2014-08-13 12:00
-    [renumeration] => volunteer
-    [application_link] => http://33.media.tumblr.com/2d95777547966a733ccdfb3e34afaacc/tumblr_n55qheEABg1qlka8ko1_400.gif
-    [job_posting_file] => http://testwestern.com/wp-content/uploads/2014/08/Governance.pdf
-    [job_description_file] => http://testwestern.com/wp-content/uploads/2014/08/Governance.pdf
-    [contact_information] => email@westernusc.ca
-        [_edit_lock] => 1407311587:1
+[_edit_last] => 1
+[job_description] => Make bread cats can't remove.
+[apply_by_date] => 2014-08-13 12:00
+[renumeration] => volunteer
+[application_link] => http://33.media.tumblr.com/2d95777547966a733ccdfb3e34afaacc/tumblr_n55qheEABg1qlka8ko1_400.gif
+[job_posting_file] => http://testwestern.com/wp-content/uploads/2014/08/Governance.pdf
+[job_description_file] => http://testwestern.com/wp-content/uploads/2014/08/Governance.pdf
+[contact_information] => email@westernusc.ca
+[_edit_lock] => 1407311587:1
  */
 
-?>
-
-<?php
 
     $array_of_meta_values =  get_post_meta( get_the_ID() );
-
-    $html_string = "";
-
-    /** @TODO: get the departments */
-
-    $subhead = "";
 
 foreach( $array_of_meta_values as $key => $value ) {
 
